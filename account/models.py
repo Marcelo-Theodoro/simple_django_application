980af0002b6ba85f2c52a755937d6635598fab71
Marcelo Theodoro <marcelotheodoro@outlook.com>
2019-01-16 20:02:13 -0200

    Implementation One

--- account/models.py
+++ account/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models, transaction
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 
@@ -9,7 +9,15 @@ class User(AbstractUser):
     name = models.CharField(max_length=128)
     country = models.CharField(max_length=2, choices=COUNTRIES)
 
+    @transaction.atomic
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if not self.business.exists():
+            Business.objects.create(user=self, is_active=True)
+
 
 class Business(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name="business", on_delete=models.CASCADE
+    )
     is_active = models.BooleanField(default=False)
--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import User
+
+
+class UserTestCase(TestCase):
+    def test_create_user_and_business(self):
+        user = User.objects.create(name="my name", country="us")
+        self.assertTrue(user.business.exists())
+
+    def test_edit_an_user(self):
+        user = User.objects.create(name="my name", country="us")
+        business = user.business.first()
+        self.assertTrue(user.business.exists())
+
+        user.name = "My other name"
+        user.save()
+        self.assertEqual(user.name, "My other name")
+        self.assertEqual(user.business.first(), business)
